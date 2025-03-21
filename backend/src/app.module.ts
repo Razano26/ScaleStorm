927161942ff2875b78e67f00ea6d4ebd69ff571a
Louis Labeyrie <labeyrielouis@gmail.com>
2025-03-21 11:39:13 +0100

    remove old node.js backend
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoadModule } from './modules/load/load.module';
-import { ReplicasModule } from './modules/replicas/replicas.module';
-import { AutoScaleModule } from './modules/autoscale/autoscale.module';
-
-@Module({
-  imports: [LoadModule, ReplicasModule, AutoScaleModule],
-})
-export class AppModule {}
--- backend/src/modules/autoscale/autoscale.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AutoScaleController } from './autoscale.controller';
-import { AutoScaleService } from './autoscale.service';
-
-@Module({
-  controllers: [AutoScaleController],
-  providers: [AutoScaleService],
-})
-export class AutoScaleModule {}
